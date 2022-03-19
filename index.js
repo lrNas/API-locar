74460d124b630182ea790193ff5cd7e47596c1bc
Marcus <pavaomarcus@gmail.com>
2022-03-19 00:24:41 -0300

    Iniciando Repositório

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const consign = require('consign')
+
+app.use(express.json())
+consign()
+    .include('./api')
+    .then('./routes')
+    .into(app)
+
+
+
+app.listen(3000,()=>{console.log('Rodando Servidor - DHLOCAR')})
